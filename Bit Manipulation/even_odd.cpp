032c11b07e70928ed0b92d40d60626429f362d71
Varun Sirpal <45942847+varun-sirpal10@users.noreply.github.com>
2020-01-21 07:33:11 +0530

    Even or odd using bitwise operators

---
+++ Bit Manipulation/even_odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    if(n&1){
+        cout<<"Odd number."<<endl;
+    }
+    else{
+        cout<<"Even number."<<endl;
+    }
+
+}
